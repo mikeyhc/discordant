a3129014a07f75862c376b0ea743b89497b4cce7
mikeyhc <mikeyhc@atmosia.net>
2021-07-02 10:25:26 -0400

    added monitor option connect/3

--- src/discordant.erl
+++ src/discordant.erl
@@ -1,13 +1,24 @@
 -module(discordant).
 
--export([connect/1, set_routes/2]).
+-export([connect/1, connect/2, set_routes/2]).
 
 -spec connect(string()) -> ok.
 connect(Token) ->
+    connect(Token, []).
+
+-spec connect(string(), [Options]) -> ok
+    when Options :: monitor.
+connect(Token, Options) ->
     ApiPid = discordant_sup:get_api_server(),
     discord_api:connect(ApiPid, Token),
     GatewayPid = discordant_sup:get_gateway(),
-    discord_gateway:connect(GatewayPid, Token).
+    discord_gateway:connect(GatewayPid, Token),
+    case lists:member(monitor, Options) of
+        true ->
+            monitor(process, discordant_sup),
+            ok;
+        false -> ok
+    end.
 
 set_routes(Msg, React) ->
     Pid = discordant_sup:get_router(),
