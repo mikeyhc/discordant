8f60cd06048ffb3f6d498596bc6feaf99c956d00
mikeyhc <mikeyhc@atmosia.net>
2022-05-12 10:06:06 -0400

    remove reconnect logging

--- src/discord_gateway.erl
+++ src/discord_gateway.erl
@@ -53,9 +53,8 @@ callback_mode() ->
 
 terminate(normal, _State, Data) ->
     cleanup(Data);
-terminate(reconnect, State, Data) ->
+terminate(reconnect, _State, Data) ->
     ?LOG_INFO("reconnect requested"),
-    ?LOG_INFO("state:data ~p:~p", [State, Data]),
     disconnect(Data#state.connection, 1001, <<"reconnect">>),
     cleanup(Data);
 terminate({shutdown, disconnected}, _State, Data) ->
